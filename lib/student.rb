7019507a58407d2dd721238e49e9372864be5217
Jimmy Andreas <jimmysignature@gmail.com>
2022-07-23 20:51:14 -0400

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require 'user'
+
+class Student < User
+
+   
+    def initialize
+        @knowledge = []
+    end
+    def learn (string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+
+    end
+
+
+
 
 end
